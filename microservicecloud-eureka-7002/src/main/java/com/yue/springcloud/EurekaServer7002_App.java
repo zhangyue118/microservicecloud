d2dc0845591388e5c4c9a7c63ad03eec3669d2e4
zhangyue118 <1143458217@qq.com>
2018-09-24 19:16:14 +0800

    test集群配置二

---
+++ microservicecloud-eureka-7002/src/main/java/com/yue/springcloud/EurekaServer7002_App.java
@@ -0,0 +1,21 @@
+package com.yue.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author: zhangyue
+ * @date: 2018/9/19 22:54
+ * @description:
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServer7002_App {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServer7002_App.class, args);
+    }
+
+}
+

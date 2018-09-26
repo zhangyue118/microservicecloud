c8cb7b5105a63f2f3f7904ebc67198b3818c98e1
zhangyue118 <1143458217@qq.com>
2018-09-26 15:25:00 +0800

    Ribbon负载均衡

---
+++ microservicecloud-provider-dept-8002/src/main/java/com/yue/springcloud/controller/DeptController.java
@@ -0,0 +1,53 @@
+package com.yue.springcloud.controller;
+
+import com.yue.springcloud.entities.Dept;
+import com.yue.springcloud.service.DeptService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+/**
+ * @author: zhangyue
+ * @date: 2018/9/19 13:55
+ * @description:
+ */
+@RequestMapping("/dept")
+@RestController
+public class DeptController {
+
+    @Autowired
+    private DeptService service;
+
+    @Autowired
+    private DiscoveryClient discoveryClient;
+
+    @PostMapping("/add")
+    public boolean add(@RequestBody Dept dept) {
+        return service.add(dept);
+    }
+
+    @GetMapping("/get/{id}")
+    public Dept get(@PathVariable("id") Long id) {
+        return service.get(id);
+    }
+
+    @RequestMapping(value = "/list", method = RequestMethod.GET)
+    public List<Dept> list() {
+        return service.list();
+    }
+
+    @GetMapping("/discovery")
+    public Object discovery() {
+        List<String> list = discoveryClient.getServices();
+        System.out.println("************" + list);
+        List<ServiceInstance> ltServiceInstance = discoveryClient.getInstances("microservicecloud-dept");
+        for (ServiceInstance s : ltServiceInstance) {
+            System.out.println(s.getServiceId() + "\t" + s.getHost() + "\t" + s.getMetadata() + "\t" + s.getPort() + "\t" + s.getUri());
+        }
+        return this.discoveryClient;
+    }
+
+}
---
+++ microservicecloud-provider-dept-8003/src/main/java/com.yue/springcloud/service/DeptService.java
@@ -0,0 +1,26 @@
+package com.yue.springcloud.service;
+
+import com.yue.springcloud.entities.Dept;
+
+import java.util.List;
+
+/**
+ * @author: zhangyue
+ * @date: 2018/9/19 13:49
+ * @description:
+ */
+public interface DeptService {
+
+    /**
+     * 添加一个部门
+     *
+     * @param dept 部门类
+     * @return boolean 是否成功
+     */
+    public boolean add(Dept dept);
+
+    public Dept get(Long id);
+
+    public List<Dept> list();
+
+}

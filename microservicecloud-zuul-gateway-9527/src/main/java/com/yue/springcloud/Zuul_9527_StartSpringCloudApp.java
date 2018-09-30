1c97860a8227b88ecc58655746e4ed7b935bf4d5
zhangyue118 <1143458217@qq.com>
2018-09-30 16:59:28 +0800

    zuul路由网关

---
+++ microservicecloud-zuul-gateway-9527/src/main/java/com/yue/springcloud/Zuul_9527_StartSpringCloudApp.java
@@ -0,0 +1,18 @@
+package com.yue.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+/**
+ * @author: zhangyue
+ * @date: 2018/9/30 15:44
+ * @description:
+ */
+@SpringBootApplication
+@EnableZuulProxy
+public class Zuul_9527_StartSpringCloudApp {
+    public static void main(String[] args) {
+        SpringApplication.run(Zuul_9527_StartSpringCloudApp.class, args);
+    }
+}
